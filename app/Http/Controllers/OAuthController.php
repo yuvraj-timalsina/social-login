2631b3a6357dd2192082e9f55ec0a63374db0c80
Yuvraj Timalsina <107990171+yuvraj-timalsina@users.noreply.github.com>
2023-01-26 20:16:44 +0545

    Update OAuthController.php

--- app/Http/Controllers/OAuthController.php
+++ app/Http/Controllers/OAuthController.php
@@ -6,38 +6,42 @@
     use Illuminate\Support\Str;
     use Illuminate\Http\Request;
     use App\Enums\OAuthProvider;
-    use Illuminate\Support\Facades\Auth;
-    use Illuminate\Support\Facades\Hash;
     use Illuminate\Http\RedirectResponse;
     use Laravel\Socialite\Facades\Socialite;
 
     class OAuthController extends Controller
     {
-        public function redirect(OAuthProvider $provider)
+        public function redirect(OAuthProvider $provider): RedirectResponse
         {
             return Socialite::driver($provider->driver())->redirect();
         }
-        
-        
+    
+    
         public function callback(OAuthProvider $provider): RedirectResponse
-
+    
         {
-            $oAuthUser = Socialite::driver($provider->driver())->user();
+            try {
+                $oAuthUser = Socialite::driver($provider->driver())->user();
             
-            $user = User::updateOrCreate([
-                'provider_id' => $oAuthUser->getId(),
-                'provider' => $provider,
-            ], [
-                'name' => $oAuthUser->getName(),
-                'email' => $oAuthUser->getEmail(),
-                'password' => Hash::make(Str::random(50)),
-                'avatar_url' => $oAuthUser->getAvatar(),
-                'provider_token' => $oAuthUser->token,
-                'provider_refresh_token' => $oAuthUser->refreshToken,
-            ]);
+                $user = User::updateOrCreate([
+                    'provider_id' => $oAuthUser->getId(),
+                    'provider' => $provider,
+                ], [
+                    'name' => $oAuthUser->getName(),
+                    'email' => $oAuthUser->getEmail(),
+                    'password' => bcrypt(Str::random(50)),
+                    'avatar_url' => $oAuthUser->getAvatar(),
+                    'provider_token' => $oAuthUser->token,
+                    'provider_refresh_token' => $oAuthUser->refreshToken,
+                ]);
             
-            Auth::login($user);
+                auth()->login($user);
             
-            return to_route('home');
+                return to_route('home');
+            }
+            catch (\Exception $e) {
+                session()->flash('error', 'Something went wrong!');
+                return to_route('welcome');
+            }
         }
     }
